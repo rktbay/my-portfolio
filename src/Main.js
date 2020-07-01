1399f29a01118bbe3b6bea40a43b0b9c4885282f
markbabaylan <matmatmark@gmail.com>
2020-07-01 19:20:07 +0800

    New simple dark layout for portfolio

--- src/Main.js
+++ src/Main.js
@@ -1,24 +1,75 @@
 import React from 'react';
 import './css/main.css';
-import linkedin from './img/linkedin.png' 
-import gmail from './img/gmail.png' 
-import phone from './img/phone.png' 
+import linkedin from './img/white-linkedin1.png' 
+import gmail from './img/white-gmail.png' 
+import github from './img/white-github1.png' 
 
+const skills = ['ReactJS', 'ASP.NET', 'C#', 'HTML', 'CSS', 'JavaScript (ES6)', 'Java', 'SQL Server', 'Oracle Database', 'MySQL', 'Adobe Photoshop', 'Microsoft Office', 'C', 'Android Studio', 'Git', 'GitHub', 'Tortoise SVN'];
+const workxp = [
+    {
+
+    },
+    {},
+    {}
+]
+const workXp = [
+    {
+        id: 1,
+        EmployerName: 'PSBank',
+        Job: 'Systems Analyst',
+        Timeline: 'Nov 2019 - July 2020'
+    },
+    {
+        id: 2,
+        EmployerName: 'Tsukiden Global Solutions Inc.',
+        Job: 'Design Engineer Trainee',
+        Timeline: 'Mar 2019 - Aug 2019'
+    },
+    {
+        id: 3,
+        EmployerName: 'AND Digital',
+        Job: 'Intern Back End Developer',
+        Timeline: 'Jan 2018 - Apr 2018'
+    }
+]
 const Main = () => {
     return (
         <>
             <nav className="navbar">
-                   <ul> 
-                       <li>Linkedin</li>
-                       <li>Gmail</li>
-                       <li>GitHub</li>
-                   </ul>
+                       <div><a href="https://www.linkedin.com/in/mbabaylan/" target="_blank"><img src={linkedin} width="22px" height="22px"/></a></div>
+                       <div><a href="https://mail.google.com/mail/?view=cm&fs=1&to=matmatmark@gmail.com" target="_blank"><img src={gmail} width="22px" height="22px"/></a></div>
+                       <div><a href="https://github.com/rakkitbay" target="_blank"><img src={github} width="22px" height="22px"/></a></div>
             </nav>
-            <h2 className="name">Hello</h2>
+            <p className="intro-text">
+                <span className="name">Hello. <br/><br/></span>
+                I'm Mark Daniel C. Babaylan. <br/>
+                And I am currently looking for Developer and Programmer job opportunities.
+            </p>
             <main>
+                <div className="skills">
+                    <h2 className="header">Skills</h2>
+                    <ul>
+                        {skills.map(skills =>{
+                            return (<li key={skills}>{skills}</li>)
+                        })}
+                    </ul>
+                </div>
+                <div className="work-exp">
+                    <h2 className="header">Work Experience</h2>
+                    
+                        {workXp.map(work=>{
+                            return(
+                            <ul>
+                                <h4>{work.EmployerName}</h4>
+                                <li>{work.Job}</li>
+                                <li>{work.Timeline}</li>
+                            </ul>
+                            )
+                        })}
+                </div>
             </main>
             <footer>
-                   {/* Created using HTML, CSS, JS and ReactJS - 2020 - Mark Babaylan */}
+                   <p>Mobile Number: 09270815174</p>
             </footer>
         </>
     )
